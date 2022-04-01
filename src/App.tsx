4d2b574aa648d1557023dc5e3e5a665eaa3d898d
薛腾飞 <xuetf@maycur.com>
2022-04-01 15:10:40 +0800

    feat: test IonAccordionGroup

--- src/App.tsx
+++ src/App.tsx
@@ -23,13 +23,17 @@ import '@ionic/react/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
+const hashHistory = require("history").createHashHistory({
+  basename: '/microReact'
+})
+
 setupIonicReact({
   // scrollAssist: false
 });
 
 const App: React.FC = () => (
   <IonApp>
-    <IonReactRouter>
+    <IonReactRouter history={hashHistory}>
       <IonRouterOutlet>
         <Route path="/" exact={true}>
           <Redirect to="/issue25018" />
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,41 +2,71 @@ import MessageListItem from '../components/MessageListItem';
 import { useState } from 'react';
 import { Message, getMessages } from '../data/messages';
 
-import { IonPage, IonContent, IonToolbar, IonTitle, IonButtons, IonBackButton, IonButton, IonIcon, IonMenuButton, IonSearchbar, IonSegment, IonSegmentButton, IonInput, IonItem,IonCheckbox, IonSelect, IonSelectOption } from '@ionic/react';
+import { IonPage, IonList, IonLabel, IonAccordion, IonAccordionGroup, IonHeader, IonContent, IonToolbar, IonTitle, IonButtons, IonBackButton, IonButton, IonIcon, IonMenuButton, IonSearchbar, IonSegment, IonSegmentButton, IonInput, IonItem,IonCheckbox, IonSelect, IonSelectOption } from '@ionic/react';
 import { personCircle, search, helpCircle, star, create, ellipsisHorizontal, ellipsisVertical } from 'ionicons/icons';
+import { useHistory } from "react-router-dom";
 
-/**
- *  for issue #25018
- *  IonItem's ionic component children inside a div doesn't work well 
- * 
- */
-
-const Home: React.FC = () => {
+const Home: React.FC = (props: any) => {
+  let history = useHistory();
 
   return (
     <IonPage>
       <IonContent>
-        <IonItem>
-          <div>
-            {/* IonCheckbox can't click */}
-            <IonCheckbox />
-            <IonSelect value={'AAA'} style={{width: 190}}>
-              <IonSelectOption>AAA</IonSelectOption>
-              <IonSelectOption>BBB</IonSelectOption>
-              <IonSelectOption>CCC</IonSelectOption>
-            </IonSelect>
-          </div>
-        </IonItem>
-
-        <IonItem>
-          {/* IonCheckbox works well */}
-          <IonCheckbox />
-          <IonSelect value={'AAA'} style={{width: 190}}>
-            <IonSelectOption>AAA</IonSelectOption>
-            <IonSelectOption>BBB</IonSelectOption>
-            <IonSelectOption>CCC</IonSelectOption>
-          </IonSelect>
-        </IonItem>
+        {/* i want colors and numbers IonAccordion default open at the same time, 
+        but it seems that it only works when the IonAccordionGroup value array has one item. */}
+        <IonAccordionGroup value={['colors', 'numbers']} multiple>
+          <IonAccordion value="colors">
+            <IonItem slot="header">
+              <IonLabel>Colors</IonLabel>
+            </IonItem>
+      
+            <IonList slot="content">
+              <IonItem>
+                <IonLabel>Red</IonLabel>
+              </IonItem>
+              <IonItem>
+                <IonLabel>Green</IonLabel>
+              </IonItem>
+              <IonItem>
+                <IonLabel>Blue</IonLabel>
+              </IonItem>
+            </IonList>
+          </IonAccordion>
+          <IonAccordion value="shapes">
+            <IonItem slot="header">
+              <IonLabel>Shapes</IonLabel>
+            </IonItem>
+      
+            <IonList slot="content">
+              <IonItem>
+                <IonLabel>Circle</IonLabel>
+              </IonItem>
+              <IonItem>
+                <IonLabel>Triangle</IonLabel>
+              </IonItem>
+              <IonItem>
+                <IonLabel>Square</IonLabel>
+              </IonItem>
+            </IonList>
+          </IonAccordion>
+          <IonAccordion value="numbers">
+            <IonItem slot="header">
+              <IonLabel>Numbers</IonLabel>
+            </IonItem>
+      
+            <IonList slot="content">
+              <IonItem>
+                <IonLabel>1</IonLabel>
+              </IonItem>
+              <IonItem>
+                <IonLabel>2</IonLabel>
+              </IonItem>
+              <IonItem>
+                <IonLabel>3</IonLabel>
+              </IonItem>
+            </IonList>
+          </IonAccordion>
+        </IonAccordionGroup>
       </IonContent>
     </IonPage>
   );
